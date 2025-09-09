e3f18d1ac3955bcfcdadfb26d77a3a59982163da
Brocolitx <grivyzom@gmail.com>
2025-09-09 18:11:58 -0300

    test: Haciendo pruebas con React.

---
+++ mockup-crea/src/componentes/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+      <header className="header">
+            <div className="container">
+                  <div className="header-content">
+                  <div className="logo">Inacap Projects</div>
+                  <div className="auth-buttons">
+                        <button className="btn btn-secondary">Iniciar Sesión</button>
+                        <button className="btn btn-primary">Registrarse</button>
+                  </div>
+                  </div>
+            </div>
+      </header>
+      );
+}
+
+export default Header;
