1a0afc6c171884054626b18fa7cf32ff7ec00111
Brocolitx <grivyzom@gmail.com>
2025-09-09 21:45:01 -0300

    Se actualiza el contenido de sección Index Se añade componente Footer Se añade componente Header

--- mockup-crea/src/App.tsx
+++ mockup-crea/src/App.tsx
@@ -1,22 +1,28 @@
 import Header from './componentes/Header'
 import Navbar from './componentes/Navbar'  // ← Agregar esta línea
+import Footer from './componentes/Footer'
 import './index.css'
 
+
+// Le digo a JavaScript: "Oye, voy a crear una función que se llama MiNuevoComponente"
 function App() {
+
+  // La función devuelve (entrega) código HTML cuando alguien la use
+  //Cuando alguien use esta receta, entrégale esto:
+  // Es como decir "el resultado final es...
   return (
     <div>
       <Header />
       <Navbar />
       <main className="main-content">
         <div className="container">
-          <section className="hero">
-            <h1>Conecta talentos, crea futuros</h1>
-            <button className="btn btn-primary">Únete Ahora</button>
-          </section>
         </div>
+      <Footer />
       </main>
     </div>
   )
 }
 
+// Permitir que otros archivos usen esta receta
+// Le digo a JavaScript: "Esta función se puede usar en otros archivos"
 export default App
---
+++ mockup-crea/src/componentes/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+      <header className="footer">
+            <div className="container">
+                  <div className="footer-content">
+                  </div>
+            </div>
+      </header>
+      );
+}
+
+export default Footer;
